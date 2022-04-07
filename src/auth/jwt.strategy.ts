a0242b1a117bde5dcd070b43637130b3a7ef51c0
haechan <kiki9510@gmail.com>
2022-04-07 18:58:52 +0900

    Added jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm/dist/common";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { UserRepository } from "./userRepository";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository: UserRepository,
+    ) {
+        super({
+            secretOrKey: "jwt-secret",
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        });
+    }
+
+    async validate(payload) {
+        const { username } = payload;
+        const user = this.userRepository.findByUserName(username);
+
+        if (!user) {
+            throw new Error(`User ${username} not found`);
+        }
+
+        return user;
+    }
+}
