1cf7304c41636743012a2282a0965e751f78d626
haechan <kiki9510@gmail.com>
2022-04-07 18:02:27 +0900

    Added typeORMConfig in root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "./auth/auth.module";
+import { typeORMConfig } from "./config/typeorm.config";
 
 @Module({
-  imports: [AuthModule],
-  controllers: [],
-  providers: [],
+    imports: [TypeOrmModule.forRoot(typeORMConfig), AuthModule],
 })
 export class AppModule {}
