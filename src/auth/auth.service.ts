0efaba2775da8000c171c1807ccbe6f6763fac6c
haechan <kiki9510@gmail.com>
2022-04-07 18:12:15 +0900

    Added createUser function in UserRepository

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { InjectRepository } from '@nestjs/typeorm';
-import { CreateAuthDto } from './dto/createAuthDto';
+import { Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { InjectRepository } from "@nestjs/typeorm";
+import { CreateAuthDto } from "./dto/createAuthDto";
+import { UserRepository } from "./userRepository";
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
-        private userRepository: UserRepository, 
-        private jwtService: JwtService
+        private userRepository: UserRepository,
+        private jwtService: JwtService,
     ) {}
 
     async createUser(createAuthDto: CreateAuthDto): Promise<void> {
-        return this.
+        return await this.userRepository.createUser(createAuthDto);
     }
 }
--- src/auth/userRepository.ts
+++ src/auth/userRepository.ts
@@ -1,1 +1,29 @@
-export class UserRepository extends Repository<User> {}
+import { EntityRepository, Repository } from "typeorm";
+import { CreateAuthDto } from "./dto/createAuthDto";
+import { User } from "./userEntity";
+import * as bcrypt from "bcryptjs";
+import {
+    ConflictException,
+    InternalServerErrorException,
+} from "@nestjs/common";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+    async createUser(createAuthDto: CreateAuthDto): Promise<void> {
+        const { username, password } = createAuthDto;
+
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const user = this.create({ username, password: hashedPassword });
+
+        try {
+            await this.save(user);
+        } catch (error) {
+            if (error.code === "23505") {
+                throw new ConflictException("Already exists username!");
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+    }
+}
